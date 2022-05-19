77a3fe6b64747c6ec04b76e28d53346b76902b5c
Mike Brewer <mike.brewer419@outlook.com>
2022-05-19 12:10:46 +0900

    fix

--- MoneyScraper/spiders/money.py
+++ MoneyScraper/spiders/money.py
@@ -59,6 +59,7 @@ class ArizonaCommitteeContribution(Model):
 db.connect()
 db.create_tables([ArizonaCandidateContribution,ArizonaCommitteeContribution])
 
+
 class MoneySpider(scrapy.Spider):
     name = 'money'
     filter_types = [130, 131, 132, 96]
